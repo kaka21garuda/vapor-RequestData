3ac9263342ae941753004d663ca3760dcb41f5cf
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-25 21:54:24 -0800

    enable post request and display data as JSON

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -12,10 +12,26 @@ struct Message: NodeRepresentable {
     
 }
 
+drop.post("create") { request in
+    guard let firstname = request.data["firstname"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    guard let secondname = request.data["secondname"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    return try! JSON(node: [
+            "firstname": "\(firstname)",
+            "secondname": "\(secondname)"
+    ])
+}
+
 drop.get("hello") { request in
     let message = Message(title: "Hello world", description: "Hello world from kaka.")
+    let anotherMessage = Message(title: "what's up!", description: "Greetings!")
     
-    return try! JSON(node: message)
+    return try! JSON(node: [message, anotherMessage])
 }
 
 drop.run()
