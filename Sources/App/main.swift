e6a24cb06fb2761944614a256d12ce2fa768a2c9
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-25 21:16:15 -0800

    display object in JSON format

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,8 +2,20 @@ import Vapor
 
 let drop = Droplet()
 
+struct Message: NodeRepresentable {
+    var title: String
+    var description: String
+    
+    func makeNode(context: Context) throws -> Node {
+        return try! Node(node: ["title": self.title, "description": self.description])
+    }
+    
+}
+
 drop.get("hello") { request in
-    return "hello world"
+    let message = Message(title: "Hello world", description: "Hello world from kaka.")
+    
+    return try! JSON(node: message)
 }
 
 drop.run()
